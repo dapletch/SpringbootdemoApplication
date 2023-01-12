04e22680663d094c9c6d84dcc4eaae5da217ff6f
Seth Pletcher <seth.pletcher@gmail.com>
2023-01-12 07:42:56 -0500

    added new Students to db using command line runner

---
+++ src/main/java/com/jpmorgan/demo/student/StudentConfig.java
@@ -0,0 +1,36 @@
+package com.jpmorgan.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student marriam = new Student(
+                    1L,
+                    "Mariam",
+                    "mariam.jamal@gmail.com",
+                    LocalDate.of(2000, Month.JANUARY, 5),
+                    21
+            );
+
+            Student alex = new Student(
+                    1L,
+                    "Alex",
+                    "alex.keeley@gmail.com",
+                    LocalDate.of(2004, Month.JANUARY, 8),
+                    22
+            );
+
+            repository.saveAll(List.of(marriam, alex));
+        };
+    }
+}
